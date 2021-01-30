1270b60c5b5d56210b9c57ba9b4eab8359a74e5b
olgaX2a <sekretaryova.olga@gmail.com>
2021-01-30 18:29:01 +0200

    UPD#7 Исходники
    дописан скрипт на меню

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,6 +1,7 @@
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
+  const mobileLogoRef = document.querySelector("[data-logo]");
 
   menuBtnRef.addEventListener("click", () => {
     const expanded =
@@ -8,6 +9,7 @@
 
     menuBtnRef.classList.toggle("is-open");
     menuBtnRef.setAttribute("aria-expanded", !expanded);
+    mobileLogoRef.classList.toggle("mobile")
 
     mobileMenuRef.classList.toggle("is-open");
   });
