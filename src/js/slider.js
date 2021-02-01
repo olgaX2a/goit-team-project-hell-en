6c11340ca7dd57f9bf37f7d0b2e046f1d44794ed
olgaX2a <sekretaryova.olga@gmail.com>
2021-02-01 11:26:55 +0200

    Fix#1
    скрипт карусели в отдельном файле
    убраны комментарии из index.html
    на месте normalize

---
+++ src/js/slider.js
@@ -0,0 +1,26 @@
+      $('.cards-list').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.bottom-nav'
+});
+$('.bottom-nav').slick({
+  slidesToShow: 7,
+  slidesToScroll: 1,
+  asNavFor: '.cards-list',
+  dots: false,
+  centerMode: false,
+  arrows: true,
+  focusOnSelect: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: true,
+        centerMode: false,
+        slidesToShow: 3
+      }
+    }
+  ]
+});
